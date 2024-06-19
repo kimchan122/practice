7e564971801f59ec8c5b45d6d2ec2e72730d0522
kimchan122 <chanchankim122@gmail.com>
2024-06-19 16:45:27 +0700

    to section2, practice

--- angular/my-first-app/src/app/todo-list-item/todo-list-item.component.ts
+++ angular/my-first-app/src/app/todo-list-item/todo-list-item.component.ts
@@ -5,7 +5,13 @@ import { Component, Input } from '@angular/core';
   selector: 'app-todo-list-item',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './todo-list-item.component.html',
+  template: `<p>{{ itemId }}: {{ itemName }}, {{ index }}</p>
+    @if(index===3){
+    <p>{{ idemId }} INDEX is 333.</p>
+    } @else if(index===4) {
+    <p>{{ itemId }} INDEX is 444.</p>
+    }
+    <p *ngIf="index === 2">{{ itemId }} INDEX is 222.</p>`,
   styleUrl: './todo-list-item.component.scss',
 })
 export class TodoListItemComponent {
